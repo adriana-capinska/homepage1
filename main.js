17b262890044689f452f248b3dbb86a5f10aa03b
Adriana Capińska <jablonska.ad@gmail.com>
2020-05-08 11:28:32 +0200

    heading__title.js

---
+++ main.js
@@ -0,0 +1,25 @@
+alert('SIEMA!!');
+console.log('WTF');
+console.log('siema');
+
+const name = 'Adriana';
+const age = '29';
+
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat.`)
+
+const heading = document.querySelector('.header__title--js');
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = `Nazywam się ${name} i mam ${age} lat.`;
+
+console.log(heading.innerHTML);
+
+console.log(heading);
+
+const navigation = document.querySelector('.header__link');
+
+console.log(navigation);
